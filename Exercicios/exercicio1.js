3952381c23ef9fe4b1e6e712bbd6b409be2ac204
Angelo dos Santos <angelokapunda@gmail.com>
2025-04-04 12:41:54 -0300

    feat : Entrega dos exercícios

---
+++ Exercicios/exercicio1.js
@@ -0,0 +1,18 @@
+
+let numeros = [2, 5, 1, 3, 4, 9, 7, 8, 10, 6];
+
+// Ordena em ordem decrescente
+numeros.sort(function(a, b) {
+  return b - a;
+});
+
+// Exibe o vetor ordenado
+console.log("Vetor em ordem decrescente:");
+console.log(numeros);
+
+
+
+
+
+
+
